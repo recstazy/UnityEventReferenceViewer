f5b1aeb6cc43a0dd7a20a06976d9bf282d98ff2b
Recstazy <djexstas9@gmail.com>
2020-06-06 17:41:50 +0700

    Moved everything into one folder

---
+++ UnityEventReferenceViewer/Example/BehaviourTwo.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace UnityEventReferenceViewer.Example
+{
+    [System.Serializable]
+    public class GenericEvent : UnityEvent<int> { }
+
+    public class BehaviourTwo : MonoBehaviour
+    {
+        #region Fields
+
+        [SerializeField]
+        private GenericEvent onExampleGenericEvent;
+
+        #endregion
+
+        #region Properties
+
+        #endregion
+
+        public void Spam()
+        {
+
+        }
+
+        public void Eggs()
+        {
+
+        }
+
+        public void IntEggs(float input)
+        {
+        }
+    }
+}
